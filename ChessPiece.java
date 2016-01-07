ed3259646b2c1608b04f661753b5794b2583bea4
Rodrigo Mejia <Rodrigo Mejia>
2016-01-07 16:40:00 -0600

    Removed Piece.java, responsibilities of coordinates now with Tile class.

--- ChessPiece.java
+++ ChessPiece.java
@@ -1,13 +1,9 @@
 package chess;
 
 public abstract class ChessPiece {
-	protected short x;
-	protected short y;
 	protected boolean isWhite;
 
-	public ChessPiece(short x, short y, boolean isWhite) {
-		this.x = x;
-		this.y = y;
+	public ChessPiece(boolean isWhite) {
 		this.isWhite = isWhite;
 	}
 
